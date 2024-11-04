427146f7065d43b48bb49f03c16a49a22ba008b0
swardiantara <swardyantara@gmail.com>
2024-11-04 12:23:21 +0700

    fix eval_data logging

--- bert_ner.py
+++ bert_ner.py
@@ -272,7 +272,7 @@ def main():
     val_loader = DataLoader(val_dataset, batch_size=16)
     _, _, all_pred_tags = ner_model.evaluate(val_loader)
     val_dataset = DroneLogDataset(test_path, ner_model.tokenizer).read_conll_file(test_path)
-    logs = log_errors_for_analysis(all_pred_tags, val_dataset.data)
+    logs = log_errors_for_analysis(all_pred_tags, val_dataset)
     with open("error_analysis_logs.json", "w") as f:
         json.dump(logs, f, indent=4)
 
