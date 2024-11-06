8dfe8f07127c6a3643641674c1e5f8d8f792952c
swardiantara <swardyantara@gmail.com>
2024-11-06 11:21:15 +0700

    script to run for training

---
+++ scripts/finetune_bert.sh
@@ -0,0 +1,12 @@
+python src/llm_fine_tune.py \
+   --model_name_or_path bert-base-cased \
+   --do_train \
+   --output_dir development \
+   --train_batch_size 16 \
+   --eval_batch_size 16 \
+   --learning_rate 2e-5 \
+   --max_seq_length 128 \
+   --train_epochs 5 \
+   --seed 42 \
+#    --align_label \
+#    --save_model \
