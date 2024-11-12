c47673a9a0d4692ad1174eae506cab3b164e19a3
swardiantara <swardyantara@gmail.com>
2024-11-12 16:20:02 +0700

    change epoch to 9

--- scripts/train_llm_rnn.sh
+++ scripts/train_llm_rnn.sh
@@ -7,7 +7,7 @@ python with_encoder.py \
    --eval_batch_size 16 \
    --learning_rate 2e-5 \
    --max_seq_length 128 \
-   --train_epochs 10 \
+   --train_epochs 9 \
    --bidirectional \
    --num_layers 2 \
    --seed 42 \
