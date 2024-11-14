f208b8f524506b2910eb6e3348faa4a56733c927
swardiantara <swardyantara@gmail.com>
2024-11-14 22:47:04 +0700

    push bert-base-cased

--- push_model.py
+++ push_model.py
@@ -1,11 +1,11 @@
 import os
-from transformers import AutoTokenizer, DistilBertForTokenClassification
+from transformers import AutoTokenizer, BertForTokenClassification
 
 # Define paths
-model_path = os.path.join("experiments", "llm-based", "distilbert-base-cased_15")
+model_path = os.path.join("experiments", "llm-based", "bert-base-cased_15")
 
 # Load your model
-model = DistilBertForTokenClassification.from_pretrained(model_path)
+model = BertForTokenClassification.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # Save it in Hugging Face's format
@@ -13,5 +13,5 @@ model.save_pretrained(model_path)
 tokenizer.save_pretrained(model_path)
 
 # Push model to Hugging Face
-model.push_to_hub("swardiantara/ADFLER-distilbert-base-cased")
-tokenizer.push_to_hub("swardiantara/ADFLER-distilbert-base-cased")
+model.push_to_hub("swardiantara/ADFLER-bert-base-cased")
+tokenizer.push_to_hub("swardiantara/ADFLER-bert-base-cased")
