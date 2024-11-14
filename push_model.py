e468c0cc18596043866aab6e314151ee4a0b62ce
swardiantara <swardyantara@gmail.com>
2024-11-14 18:47:20 +0700

    push xlnet-base-cased

--- push_model.py
+++ push_model.py
@@ -1,11 +1,11 @@
 import os
-from transformers import RobertaForTokenClassification, AutoTokenizer
+from transformers import XLNetForTokenClassification, AutoTokenizer
 
 # Define paths
-model_path = os.path.join("experiments", "llm-based", "roberta-base_15")
+model_path = os.path.join("experiments", "llm-based", "xlnet-base-cased_15")
 
 # Load your model
-model = RobertaForTokenClassification.from_pretrained(model_path)
+model = XLNetForTokenClassification.from_pretrained(model_path)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 # Save it in Hugging Face's format
@@ -13,5 +13,5 @@ model.save_pretrained(model_path)
 tokenizer.save_pretrained(model_path)
 
 # Push model to Hugging Face
-model.push_to_hub("swardiantara/ADFLER-Roberta-base")
-tokenizer.push_to_hub("swardiantara/ADFLER-Roberta-base")
+model.push_to_hub("swardiantara/ADFLER-xlnet-base-cased")
+tokenizer.push_to_hub("swardiantara/ADFLER-xlnet-base-cased")
