e077da2512491acaf8e675b025a4b5b68bdc8700
swardiantara <swardyantara@gmail.com>
2024-11-14 16:56:45 +0700

    try pushing models to Huggingface Hub

---
+++ push_model.py
@@ -0,0 +1,17 @@
+import os
+from transformers import AlbertForTokenClassification, AutoTokenizer
+
+# Define paths
+model_path = os.path.join("experiments", "llm-based", "albert-base-v2_15")
+
+# Load your model
+model = AlbertForTokenClassification.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+# Save it in Hugging Face's format
+model.save_pretrained(model_path)
+tokenizer.save_pretrained(model_path)
+
+# Push model to Hugging Face
+model.push_to_hub("swardiantara/ADFLER-albert-base-v2")
+tokenizer.push_to_hub("swardiantara/ADFLER-albert-base-v2")
