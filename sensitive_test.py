e21434aaea01479cee842f174652d9e49e599bf1
swardiantara <swardyantara@gmail.com>
2024-11-22 17:35:53 +0700

    add more samples to test

--- sensitive_test.py
+++ sensitive_test.py
@@ -9,8 +9,11 @@ model = pipeline('ner', model=model_path)
 samples = [
     "Tap Fly Flight Ended. Landing Gear Lowered.",
     "Tap Fly Flight Ended Landing Gear Lowered.",
-    "Tap Fly Flight Ended, Landing Gear Lowered.",
-    "Tap Fly Flight Ended: Landing Gear Lowered.",
+    "Precision Landing. Correcting Landing Position.",
+    "Precision Landing Correcting Landing Position.",
+    "Propeller Fell Off. Motor idle. Check whether propellers are installed.",
+    "Propeller Fell Off Motor idle Check whether propellers are installed.",
+    "Return-to-Home Altitude: 98FT.",
 ]
 
 for sample in samples:
