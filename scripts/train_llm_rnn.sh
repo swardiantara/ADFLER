0c20800fbbdf1f0b39faf1fda640fab3f04c3347
swardiantara <swardyantara@gmail.com>
2024-11-24 12:21:56 +0700

    try albert-base-v2 with BiLSTM-2

--- scripts/train_llm_rnn.sh
+++ scripts/train_llm_rnn.sh
@@ -1,13 +1,13 @@
 python with_encoder.py \
-    --model_type roberta \
-   --model_name_or_path roberta-base \
+    --model_type albert \
+   --model_name_or_path albert/albert-base-v2 \
    --encoder lstm \
    --output_dir experiments \
    --train_batch_size 16 \
    --eval_batch_size 16 \
    --learning_rate 2e-5 \
    --max_seq_length 128 \
-   --train_epochs 9 \
+   --train_epochs 15 \
    --bidirectional \
    --num_layers 2 \
    --seed 42 \
