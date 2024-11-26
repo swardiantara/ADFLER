cfe4547caa4b54d93f497757c71decd87c285a98
swardiantara <swardyantara@gmail.com>
2024-11-26 20:10:19 +0700

    test roberta's robustness on lower-cased samples

---
+++ scripts/roberta_lower.sh
@@ -0,0 +1,36 @@
+rm -r cache_dir
+python finetune.py --do_eval --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
+
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
+
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
+
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
+
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
+
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset low1 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset low2 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset low3 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset low4 --model_type roberta --model_name_or_path roberta-base
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset low5 --model_type roberta --model_name_or_path roberta-base
