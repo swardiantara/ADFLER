a5e528c48aebbba14f2700bc78d1b56f0d702a27
swardiantara <swardyantara@gmail.com>
2024-11-26 17:32:42 +0700

    fix evaluate albert on original and punct remove robustness

--- scripts/albert_aug.sh
+++ scripts/albert_aug.sh
@@ -1,17 +1,72 @@
 rm -r cache_dir
-python finetune.py --do_train --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
 
-rm -r cache_dir
-python finetune.py --do_train --train_dataset rem-100 --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
 
-rm -r cache_dir
-python finetune.py --do_train --train_dataset rem-80 --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
 
-rm -r cache_dir
-python finetune.py --do_train --train_dataset rem-60 --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
 
-rm -r cache_dir
-python finetune.py --do_train --train_dataset rem-40 --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
 
-rm -r cache_dir
-python finetune.py --do_train --train_dataset rem-20 --do_eval --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset ori1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset ori2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset ori3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset ori4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset ori5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-100 --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-80 --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-60 --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-40 --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
+
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset rem1 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset rem2 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset rem3 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset rem4 --model_type albert --model_name_or_path albert/albert-base-v2
+python finetune.py --do_eval --train_dataset rem-20 --eval_dataset rem5 --model_type albert --model_name_or_path albert/albert-base-v2
