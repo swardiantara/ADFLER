7353da11b4c08d948109cca62dd50cfa4f496246
swardiantara <swardyantara@gmail.com>
2024-11-27 16:04:54 +0700

    try using bert-uncased variant

--- scripts/finetune_bert.sh
+++ scripts/finetune_bert.sh
@@ -1,13 +1,1 @@
-python bert_ner.py \
-   --model_name_or_path bert-base-cased \
-   --do_train \
-   --do_direct_eval \
-   --output_dir development \
-   --train_batch_size 16 \
-   --eval_batch_size 16 \
-   --learning_rate 2e-5 \
-   --max_seq_length 128 \
-   --train_epochs 5 \
-   --seed 42 \
-#    --align_label \
-#    --save_model \
+python finetune.py --do_train --do_eval --model_type bert --model_name_or_path bert-base-uncased
